3b9bd0c540555d525baf33ecc6ff9e4b485f821a
Stewart <44583600+stewheart123@users.noreply.github.com>
2022-08-20 21:57:17 +0100

    before game controller

--- resources/views/games-index.blade.php
+++ resources/views/games-index.blade.php
@@ -2,6 +2,10 @@
 @section('games-index')
 <h1>games</h1>
 @foreach ($all_public_games as $game)
-<p>{{$game->GM_TITLE}}</p>
+<div class="game-container card">
+    <p>{{$game->GM_TITLE}}</p>
+    <p>{{$game->GM_DESCRIPTION}}</p>
+
+</div>
 @endforeach
 @endsection
